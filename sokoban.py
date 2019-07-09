ad930d0dee2eda78aef71743b08d327cfcf27209
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-07-09 08:57:05 -0500

    base del mapa

---
+++ sokoban.py
@@ -0,0 +1,28 @@
+"""
+0-personaje
+1-cajas
+2-paredes
+3-metas
+4-pasillos
+5-caja/meta
+"""
+#map=[2,4,4,4,0,4,4,1,3,2]
+#print(map)
+map=[2,4,4,4,0,4,4,4,2]
+position_x=4
+#position_x=position_x+1
+#right
+#position_x=position_x-1
+#left
+while True:
+    print (map)
+    move=raw_input("D-right,A-Left")
+    tem_x=position_x
+    #tem_x guarda la posicion ddel personaje
+    if move=='d' and map[position_x+1]!=2:
+        position_x=position_x+1
+    elif move=='a':
+         position_x=position_x-1
+    map[tem_x]=4
+    map[position_x]=0
+
