31975a9d32df7099de41498daf487755ba31dfac
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-07-10 15:00:27 -0500

    Add files via upload

---
+++ sokoban2.py
@@ -0,0 +1,28 @@
+mapa=[
+        [2,2,2,2,2],
+        [2,4,0,4,2],
+        [2,4,4,4,2],
+        [2,2,2,2,2]]
+position_row=1
+position_col=2
+mapa[position_row][position_col]=0
+print(mapa)
+"""while True:
+    print(mapa)
+    move_row=raw_input("D-right,A-Left")
+tem_row=position_row
+if move_row=='d' and mapa[position_row+1]!=2:
+    #right
+        position_row=position_row+1
+elif move_row=='a'and mapa[position_col-1]!=2:
+    #left
+         position_col=position_col-1
+tem_col=position_col
+move_col=raw_input("W-Up,S-Down")
+if move_col=='w' and mapa[position_col-1]!=2:
+    #right
+        position_col=position_col-1
+elif move_col=='s'and mapa[position_col+1]!=2:
+    #left
+         position_col=position_col+1"""
+
