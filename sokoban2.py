3f2e6d7cf60eaa32b465dfe21256a53126963811
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-07-11 09:58:51 -0500

    Add files via upload

--- sokoban2.py
+++ sokoban2.py
@@ -1,46 +1,40 @@
+#se mueve pero solo imprime una linea
 mapa=[
         [2,2,2,2,2],
         [2,4,0,4,2],
         [2,4,4,4,2],
         [2,2,2,2,2]]
-#FUNCIONA
+#Crea las tablas
 j=0
 for a in range(len(mapa)):
     print(mapa[j])
     j=j+1
-
-
-#intento 1:sigue marca error (he buscado en internet sin exito)
-"""
-for var in mapa:
-    for var in jar:
-        print(var, end=', ')
-"""
-#intento 2: Funciona pero se debe hacer de forma manual
-"""
-print (mapa[1])
-print (mapa[2])
-print (mapa[3])
-"""
-
-#codigo de movimientos
 position_row=1
 position_col=2
+mapa[position_row][position_col]=0
+caja=4
 while True:
-    print(mapa)
-    move_row=raw_input("D-right,A-Left")
+   
+    move_row=raw_input("D-right,A-Left,W-Up,S-Down")
 tem_row=position_row
-if move_row=='d' and mapa[position_row+1]!=2:
+tem_col=position_col
+
+if move_row=='d' and mapa[position_row+1]!=2 and map[caja+1]!=2:
     #right
         position_row=position_row+1
-elif move_row=='a'and mapa[position_col-1]!=2:
+elif move_row=='a'and mapa[position_col-1]!=2 and map[caja+1]!=2:
     #left
          position_col=position_col-1
-tem_col=position_col
-move_col=raw_input("W-Up,S-Down")
-if move_col=='w' and mapa[position_col-1]!=2:
-    #right
+move_col=raw_input("D-right,A-Left,W-Up,S-Down")
+if move_col=='w' and mapa[position_col-1]!=2 and map[caja+1]!=2:
+    #up
         position_col=position_col-1
-elif move_col=='s'and mapa[position_col+1]!=2:
-    #left
-         position_col=position_col+1
+elif move_col=='s'and mapa[position_col+1]!=2 and map[caja+1]!=2: 
+    #down
+         position_col=position_col+1 
+mapa[tem_col]=4
+mapa[tem_row]=4
+mapa[position_col]=0
+mapa[position_row]=0
+mapa[caja]=4
+
