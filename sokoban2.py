46b2f7b0247907ce28cedfbd0e38947d8b4d847b
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-07-23 08:25:34 -0500

    Add files via upload
    movimiento izquierda y derecha

--- sokoban2.py
+++ sokoban2.py
@@ -24,8 +24,8 @@ class Instructions:
                 [2,2,2,2,2]]
        
 #imprimir el mapa
-    def mapa (self):
-        
+    
+    def forma_mapa(self):  
         j=0
         for j in range(len(self.mapa)):
             print(self.mapa[j])
@@ -37,7 +37,7 @@ class Instructions:
 
                 if self.mapa[j][i]==0:
                     self.position_col=j
-                    self position_row=i
+                    self.position_row=i
                     self.tem_col=self.position_col
                     self.tem_row=self.position_row
 
@@ -49,16 +49,16 @@ class Instructions:
             self.mapa[self.tem_row] [self.tem_col]=4
             self.tem_row=self.position_row
             self.tem_col=self.position_col
-            elif self.mapa[self.position_raw][self.position_col-1]!=2 and self.maps[self.position_raw][self.position_col-1]==1 and self.maps[self.position_raw][self.position_col-2]!=2 and self.maps[self.position_raw][self.position_col-2]!=1: 
+        elif self.mapa[self.position_row][self.position_col-1]!=2 and self.mapa[self.position_row][self.position_col-1]==1: 
             self.position_col=self.position_col-1
             self.caja_col=self.position_col-1
             self.caja_row=self.position_row
             #movimiento que empuja la caja
-            self.mapa[self.position_raw][self.position_col]=0   
+            self.mapa[self.position_row][self.position_col]=0   
             self.mapa[self.tem_row][self.tem_col]=4
             self.mapa[self.caja_row][self.caja_col]=1
             self.tem_row=self.position_row
-            self.tem_col=self.position_col   
+            self.tem_col=self.position_col
 #movimiento derecha
     def move_right (self):
         if self.mapa[self.position_col+1]!=2 and self.mapa[self.position_col][self.position_row+1]!=1:
@@ -67,14 +67,27 @@ class Instructions:
             self.mapa[self.tem_row][self.tem_row]=4
             self.tem_row=self.position_row
             self.tem_col=self.position_col
-            elif self.mapa
-           
+        elif self.mapa[self.position_row][self.position_col+1]!=2 and self.mapa[self.position_col][self.position_row+1]==1:
+            self.position_col=self.position_col+1
+            self.caja_col=self.position_col+1
+            self.caja_row=self.position_row
+            #movimiento que empuja la caja
+            self.mapa[self.position_row][self.position_col]=0
+            self.mapa[self.tem_row][self.tem_col]=4
+            self.mapa[self.caja_row][self.caja_col]=1
+            self.tem_row=self.position_row
+            self.tem_col=self.position_col
+ 
+         
 #movimiento arriba
     def move_up (self):
-         if self.mapa[self.position_row+1]==1:
-            tem_row=self.position_row
+          if self.mapa[self.position_row-1][self.position_col]!=2 and self.mapa[self.position_row-1][self.position_col]!=1:
             self.position_row=self.position_row-1
-            self.mapa[tem_row]=1
+            self.mapa[self.position_row][self.position_col]=0
+            self.mapa[tem_row][tem_col]=4
+            self.tem_raw=self.position_raw
+            self.tem_col=self.position_col
+    elif self.maps[self.position_raw-1][self.position_col]!=2 and self.maps[self.position_raw-1][self.position_col]==1 and self.maps[self.position_raw-2][self.position_col]!=2 and self.maps[self.position_raw-2][self.position_col]!=1: 
             self.mapa[self.position_col and self.position_row]=0
 #movimiento abajo 
     def move_down (self):
@@ -85,22 +98,24 @@ class Instructions:
             self.mapa[self.position_col and self.position_row]=0
     def caja (self):
         while True:
-            self.printmapa()
-            
+            self.mapa()
+ 
     
 
 #inicio de juego
     def juego (self):
-        self.crea_mapa()
+        self.forma_mapa()
         self.e_personaje()
         while True:
-            self.printmapa()
+            self.mapa()
             move=raw_input("D-right,A-Left,W-Up,S-Down")
             if move=='a':
                 self.move_left()
             elif move=='d':
                 self.move_right()
+"""
             elif move=='w':
                 self.move_up()
             elif move=='s':
                 self.move_down()
+"""
