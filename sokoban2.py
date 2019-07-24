36fcf758a770d1018dd0a10a0f93935df8e30656
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-07-24 13:58:18 -0500

    revisar los objetos

--- sokoban2.py
+++ sokoban2.py
@@ -53,7 +53,7 @@ class Instructions:
             self.position_col=self.position_col-1
             self.caja_col=self.position_col-1
             self.caja_row=self.position_row
-            #movimiento que empuja la caja
+#mueve la caja
             self.mapa[self.position_row][self.position_col]=0   
             self.mapa[self.tem_row][self.tem_col]=4
             self.mapa[self.caja_row][self.caja_col]=1
@@ -71,7 +71,7 @@ class Instructions:
             self.position_col=self.position_col+1
             self.caja_col=self.position_col+1
             self.caja_row=self.position_row
-            #movimiento que empuja la caja
+#mueve la caja
             self.mapa[self.position_row][self.position_col]=0
             self.mapa[self.tem_row][self.tem_col]=4
             self.mapa[self.caja_row][self.caja_col]=1
@@ -83,39 +83,53 @@ class Instructions:
     def move_up (self):
         if self.mapa[self.position_row-1][self.position_col]!=2 and self.mapa[self.position_row-1][self.position_col]!=1:
             self.position_row=self.position_row-1
-            self.mapa[self.position_row][self.position_col]=0
+            self.mapa[self.position_row][self.position_col]=0   #camina arriba
             self.mapa[self.tem_row][self.tem_col]=4
             self.tem_row=self.position_row
             self.tem_col=self.position_col
         elif self.mapa[self.position_row-1][self.position_col]!=2 and self.mapa[self.position_row-1][self.position_col]==1 and self.mapa[self.position_row-2][self.position_col]!=2 and self.mapa[self.position_row-2][self.position_col]!=1: 
-            self.mapa[self.position_col and self.position_row]=0
+            self.position_row=self.position_row-1
+            self.caja_row=self.position_row-1
+            self.caja_col=self.position_col
+#mueve la caja
+            self.mapa[self.position_row][self.position_col]=0   
+            self.mapa[self.tem_row][self.tem_col]=4
+            self.mapa[self.caja_row][self.caja_col]=1
+            self.tem_row=self.position_row
+            self.tem_col=self.position_col      
 #movimiento abajo 
     def move_down (self):
-        if self.mapa[self.position_row+1]==1:
-            tem_row=self.position_row
+        if self.mapa[self.position_row+1][self.position_col]!=2:
             self.position_row=self.position_row+1
-            self.mapa[tem_row]=1
-            self.mapa[self.position_col and self.position_row]=0
-    def caja (self):
-        while True:
-            self.mapa()
- 
-    
-
+            self.mapa[self.position_row][self.position_col]=0
+            self.mapa[self.tem_row][self.tem_col]=4
+            self.tem_row=self.position_row
+            self.tem_col=self.position_col
+        elif self.mapa[self.position_row+1][self.position_col]!=2 and self.mapa[self.position_row+1][self.position_col]==1 and self.mapa[self.position_row+2][self.position_col]!=2 and self.mapa[self.position_row+2][self.position_col]!=1: 
+            self.position_row=self.position_row+1
+            self.caja_row=self.position_row+1
+            self.caja_col=self.position_col
+#mueve la caja            
+            self.mapa[self.position_row] [self.position_col]=0
+            self.mapa[self.tem_row][self.tem_col]=4
+            self.mapa[self.caja_row][self.tem_col]=1
+            self.tem_row=self.position_row
+            self.tem_col=self.position_col
 #inicio de juego
     def juego (self):
+        self.crea_mapa()
         self.forma_mapa()
         self.e_personaje()
         while True:
-            self.mapa()
+            self.crea_mapa()
             move=raw_input("D-right,A-Left,W-Up,S-Down")
             if move=='a':
                 self.move_left()
             elif move=='d':
                 self.move_right()
-"""
             elif move=='w':
                 self.move_up()
             elif move=='s':
                 self.move_down()
-"""
+sphinx=Instructions()
+sphinx.juego()
