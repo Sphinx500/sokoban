28d987633a8cbc5c30e7e265b9c422047934d5b6
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-08-06 09:00:26 -0500

    contador de cajas y final

--- prueba.py
+++ prueba.py
@@ -6,7 +6,7 @@
 4-Pasillos
 5-Caja/meta
 """
-class Game:
+class Sokoban:
     def __init__(self):
         self.maps=[[]]
         self.position_row=0
@@ -15,20 +15,20 @@ class Game:
         self.tem_col=0
         self.caja_row=0
         self.caja_col=0
-        self.pared_col=0
-        self.pared_row=0
+        self.meta_col=0
+        self.meta_row=0
     def crear(self):
         self.maps=[
-        [2,2,2,2,2,2,2,2,2,2,2,2],
-        [2,4,4,4,4,4,4,4,4,4,4,2],
-        [2,0,4,4,4,4,4,4,4,4,4,2],
-        [2,4,4,4,4,4,4,4,4,4,4,2],
-        [2,4,4,4,4,4,4,4,4,4,4,2],
-        [2,2,2,2,4,4,4,4,4,4,4,2]]
-       
-#imprimir el mapa
-    
+        [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+        [2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2],
+        [2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2],
+        [2,4,3,1,4,4,4,0,4,4,4,4,1,3,4,2],
+        [2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2],
+        [2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2],
+        [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]]
+        
     def mapa(self):
+        
         cont=0
         for cont in range(len(self.maps)):
             print(self.maps[cont])
@@ -42,96 +42,126 @@ class Game:
                     self.tem_row=self.position_row
                     self.tem_col=self.position_col
 
-#IZQUIERDA
-    def move_left (self):
-        if self.maps[self.position_row][self.position_col-1]==4:
-            self.tem_col=self.position_col
-            self.position_col=self.position_col-1
-            self.maps[self.tem_row][self.tem_col]=4
-            self.maps[self.position_col][self.position_row]=0
-            #mueve la caja
-        elif self.maps[self.position_row][self.position_col-1]!=2 and self.maps[self.position_row][self.position_col-1]==1 and self.maps[self.position_row][self.position_col-2]!=2 and self.maps[self.position_row][self.position_col-2]!=1: 
-            self.position_col=self.position_col-1
-            self.caja_col=self.position_col-1
-            self.caja_row=self.position_row
+
+    def derecha(self):
+#DERECHA
+        if self.maps[self.position_row][self.position_col+1]== 4:
+            self.position_col=self.position_col+1
             self.maps[self.position_row][self.position_col]=0
             self.maps[self.tem_row][self.tem_col]=4
-            self.maps[self.caja_row][self.caja_col]=1
-#chocar con pared
-        elif self.maps[self.position_row][self.position_col-1]==2:
-            self.maps[self.position_row][self.position_col]=0
             self.tem_row=self.position_row
             self.tem_col=self.position_col
-        
-
-#DERECHA
-    def move_right (self):
-        if self.maps[self.position_row][self.position_col+1]==4:
-            self.position_col=self.position_col+1
-            self.maps[self.tem_row][self.tem_col]=4
-            self.maps[self.position_col][self.position_row]=0
-            self.tem_col=self.position_col
-            #mueve la caja
-        elif self.maps[self.position_row][self.position_col+1]!=2 and self.maps[self.position_row][self.position_col+1]==1 and self.maps[self.position_row][self.position_col+2]!=2 and self.maps[self.position_row][self.position_col+2]!=1 :
+#mueve la caja
+        elif self.maps[self.position_row][self.position_col+1]==1 and self.maps[self.position_row][self.position_col+2]==4: 
             self.position_col=self.position_col+1
             self.caja_col=self.position_col+1
             self.caja_row=self.position_row
-
-            self.maps[self.position_row][self.position_col]=0
+            self.maps[self.position_row][self.position_col]=0   
             self.maps[self.tem_row][self.tem_col]=4
             self.maps[self.caja_row][self.caja_col]=1
             self.tem_row=self.position_row
             self.tem_col=self.position_col
-        #chocar con pared
-        elif self.maps[self.position_row][self.position_col+1]==2:
+#meta 
+        elif self.maps[self.position_row][self.position_col+1]==1 and self.maps[self.position_row][self.position_col+2]==3:
+             self.position_col=self.position_col+1
+             self.maps[self.position_row][self.position_col]=0 
+             self.maps[self.position_row][self.position_col+1]=5
+             self.maps[self.position_row][self.position_col-1]=4
+             self.tem_row=self.position_row
+             self.tem_col=self.position_col
+           
+    
+    def izquierda(self):
+#IZQUIERDA
+        if self.maps[self.position_row][self.position_col-1]== 4:
+            self.position_col=self.position_col-1
             self.maps[self.position_row][self.position_col]=0
+            self.maps[self.tem_row][self.tem_col]=4
             self.tem_row=self.position_row
             self.tem_col=self.position_col
-#ARRIBA
-    def move_up (self):
-        if self.maps[self.position_row-1][self.position_col]==4:
-             self.position_row=self.position_row-1
-             self.maps[self.tem_row][self.tem_col]=4
-             self.maps[self.position_col][self.position_row]=0
+#mueve la caja
+        elif self.maps[self.position_row][self.position_col-1]==1 and self.maps[self.position_row][self.position_col-2]==4: 
+            self.position_col=self.position_col-1
+            self.caja_col=self.position_col-1
+            self.caja_row=self.position_row
+            self.maps[self.position_row][self.position_col]=0   
+            self.maps[self.tem_row][self.tem_col]=4
+            self.maps[self.caja_row][self.caja_col]=1
+            self.tem_row=self.position_row
+            self.tem_col=self.position_col
+        #meta 
+        elif self.maps[self.position_row][self.position_col-1]==1 and self.maps[self.position_row][self.position_col-2]==3:
+             self.position_col=self.position_col-1
+             self.maps[self.position_row][self.position_col]=0 
+             self.maps[self.position_row][self.position_col-1]=5
+             self.maps[self.position_row][self.position_col+1]=4
              self.tem_row=self.position_row
-        elif self.maps[self.position_row-1][self.position_col]!=2 and self.maps[self.position_row-1][self.position_col]==1 and self.maps[self.position_row-2][self.position_col]!=2 and self.maps[self.position_row-2][self.position_col]!=1: 
+             self.tem_col=self.position_col
+
+
+#MOVIMIENTO ARRIBA
+    def arriba(self):
+        if self.maps[self.position_col][self.position_row-1]== 4:
+            self.position_row=self.position_row-1
+            self.maps[self.position_row][self.position_col]=0
+            self.maps[self.tem_row][self.tem_col]=4
+            self.tem_row=self.position_row
+            self.tem_col=self.position_col
+#mueve la caja
+        elif self.maps[self.position_row-1][self.position_col]==1 and self.maps[self.position_row-2][self.position_col]==4: 
             self.position_row=self.position_row-1
             self.caja_row=self.position_row-1
             self.caja_col=self.position_col
-#mueve la caja
-            self.maps[self.position_row][self.position_col]=0
+            self.maps[self.position_row][self.position_col]=0 
             self.maps[self.tem_row][self.tem_col]=4
             self.maps[self.caja_row][self.caja_col]=1
             self.tem_row=self.position_row
             self.tem_col=self.position_col
-        #chocar con pared
-        elif self.maps[self.position_row-1][self.position_col]==2:
-            self.maps[self.position_row][self.position_col]=0
-            self.tem_row=self.position_row
-            self.tem_col=self.position_col
+#meta 
+
+    def abajo(self):
 #ABAJO
-    def move_down (self):
-        if self.maps[self.position_row+1][self.position_col]==4:
-             self.position_row=self.position_row+1
-             self.maps[self.tem_row][self.tem_col]=4
-             self.maps[self.position_col][self.position_row]=0
-             self.tem_row=self.position_row
-        elif self.maps[self.position_row+1][self.position_col]!=2 and self.maps[self.position_row+1][self.position_col]==1 and self.maps[self.position_row+2][self.position_col]!=2 and self.maps[self.position_row+2][self.position_col]!=1: 
+        if self.maps[self.position_row-1][self.position_col]== 4:
             self.position_row=self.position_row+1
-            self.caja_row=self.position_row+1
-            self.caja_col=self.position_col
-#mueve la caja            
-            self.maps[self.position_row] [self.position_col]=0
-            self.maps[self.tem_row][self.tem_col]=4
-            self.maps[self.caja_row][self.tem_col]=1
-            self.tem_row=self.position_row
-            self.tem_col=self.position_col
-            #chocar con pared
-        elif self.maps[self.position_row+1][self.position_col]==2:
             self.maps[self.position_row][self.position_col]=0
+            self.maps[self.tem_row][self.tem_col]=4
             self.tem_row=self.position_row
             self.tem_col=self.position_col
-#para iniciar el juego    
+#mueve la caja
+        elif self.maps[self.position_row][self.position_col+1]==1 and self.maps[self.position_row][self.position_col+2]==4: 
+            self.position_col=self.position_col+1
+            self.caja_col=self.position_col+1
+            self.caja_row=self.position_row
+            self.maps[self.position_row][self.position_col]=0   
+            self.maps[self.tem_row][self.tem_col]=4
+            self.maps[self.caja_row][self.caja_col]=1
+            self.tem_row=self.position_row
+            self.tem_col=self.position_col
+
+    def cont_cajas (self):
+        cont=0
+        for caja in range(len(self.maps)):
+            print(self.maps[caja])
+            caja=caja+1
+        
+
+#ERRORES Y EXCEPCIONES
+    def errores(self):
+        if move=!"A" or move=!"a":
+            print("ESTA TECLA ES INCORRECTA")
+        elif move=! "D" or move=!"d":
+            print("ESTA TECLA ES INCORRECTA")
+        elif move=! "S" or move=!"s":
+            print("ESTA TECLA ES INCORRECTA")
+        elif move=! "W" or move=!"w":
+            print("ESTA TECLA ES INCORRECTA")
+#TERMINAR NIVEL
+    def finish(self):
+        if 
+
+
+
+        
     def jugar(self):
         self.crear()
         self.posicion()
@@ -139,15 +169,15 @@ class Game:
             self.mapa()
             move=raw_input("D-right,A-left,W-up,S-down")
             if move == "d" or move=="D" :
-                self.move_right()
+                self.derecha()
             elif move=="a" or move=="A":
-                self.move_left()
-            elif move=="w" or move=="W" :
-                self.move_up()
-            elif move=="s" or move=="S" :
-                self.move_down()
+                self.izquierda()
+            elif move=="s" or move=="S":
+                self.abajo()
+            elif move=="w" or move=="W":
+                self.arriba()
 
-sphinx=Game()
+
+sphinx=Sokoban()
 sphinx.jugar()
-
 
