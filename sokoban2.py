807d3fe6fc6a83e3692e2d19d4c0ea5fcc1af47f
FernandoHernandezVazquez <50550859+Sphinx500@users.noreply.github.com>
2019-08-07 14:55:42 -0500

    saca la caja de la meta

--- sokoban2.py
+++ sokoban2.py
@@ -5,7 +5,9 @@
 3-Metas
 4-Pasillos
 5-Caja/meta
+6-Personaje/Meta
 """
+
 class Sokoban:
     def __init__(self):
         self.maps=[[]]
@@ -17,14 +19,16 @@ class Sokoban:
         self.caja_col=0
         self.meta_col=0
         self.meta_row=0
+        self.caja=0
+        self.personaje_meta=0
     def crear(self):
         self.maps=[
         [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
-        [2,4,4,4,4,4,4,3,4,4,4,4,4,4,4,2],
-        [2,4,4,4,4,4,4,1,4,4,4,4,4,4,4,2],
-        [2,4,4,4,4,3,1,0,1,3,4,4,4,4,4,2],
-        [2,4,4,4,4,4,4,1,4,4,4,4,4,4,4,2],
-        [2,4,4,4,4,4,4,3,4,4,4,4,4,4,4,2],
+        [2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2],
+        [2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2],
+        [2,4,3,1,4,4,4,0,4,1,3,4,4,4,4,2],
+        [2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2],
+        [2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,2],
         [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]]
         
     def mapa(self):
@@ -61,13 +65,31 @@ class Sokoban:
             self.maps[self.caja_row][self.caja_col]=1
             self.tem_row=self.position_row
             self.tem_col=self.position_col
+#saca la caja de la meta
+        elif self.maps[self.position_row][self.position_col]==0 and self.maps[self.position_row][self.position_col+1]==5 and self.maps[self.position_row][self.position_col+2]==4:
+            self.position_col=self.position_col+1
+            self.caja_col=self.caja_col+1
+            self.maps[self.position_row][self.position_col]=6 
+            self.maps[self.position_row][self.position_col+1]=1
+            self.maps[self.tem_row][self.tem_col]=4
+            self.tem_row=self.position_row
+            self.tem_col=self.position_col
+        elif self.maps[self.position_row][self.position_col]==6 and self.maps[self.position_row][self.position_col+1]==1 and self.maps[self.position_row][self.position_col+2]==4:
+            self.position_col=self.position_col+1
+            self.caja_col=self.caja_col+1
+            self.maps[self.position_row][self.position_col]=0
+            self.maps[self.position_row][self.position_col+1]=1
+            self.maps[self.tem_row][self.tem_col]=3
+            self.tem_row=self.position_row
+            self.tem_col=self.position_col
 #meta 
-        elif self.maps[self.position_row][self.position_col+1]==1 and self.maps[self.position_row][self.position_col+2]==3:
+        elif self.maps[self.position_row][self.position_col]==0 and self.maps[self.position_row][self.position_col+1]==1 and self.maps[self.position_row][self.position_col+2]==3:
              self.position_col=self.position_col+1
              self.maps[self.position_row][self.position_col]=0 
              self.maps[self.position_row][self.position_col+1]=5
              self.maps[self.position_row][self.position_col-1]=4
-
+             self.tem_row=self.position_row
+             self.tem_col=self.position_col
            
     
     def izquierda(self):
@@ -94,6 +116,10 @@ class Sokoban:
              self.maps[self.position_row][self.position_col]=0 
              self.maps[self.position_row][self.position_col-1]=5
              self.maps[self.position_row][self.position_col+1]=4
+             self.tem_row=self.position_row
+             self.tem_col=self.position_col
+
+
 #MOVIMIENTO ARRIBA
     def arriba(self):
         if self.maps[self.position_row-1][self.position_col]== 4:
@@ -112,12 +138,15 @@ class Sokoban:
             self.maps[self.caja_row][self.caja_col]=1
             self.tem_row=self.position_row
             self.tem_col=self.position_col
+        
 #meta 
         elif self.maps[self.position_row-1][self.position_col]==1 and self.maps[self.position_row-2][self.position_col]==3:
              self.position_row=self.position_row-1
              self.maps[self.position_row][self.position_col]=0 
              self.maps[self.position_row-1][self.position_col]=5
              self.maps[self.position_row+1][self.position_col]=4
+             self.tem_row=self.position_row
+             self.tem_col=self.position_col
     
     def abajo(self):
 #ABAJO
@@ -141,8 +170,16 @@ class Sokoban:
         elif self.maps[self.position_row+1][self.position_col]==1 and self.maps[self.position_row+2][self.position_col]==3:
              self.position_row=self.position_row+1
              self.maps[self.position_row][self.position_col]=0 
-             self.maps[self.position_row][self.position_col+1]=5
-             self.maps[self.position_row][self.position_col-1]=4
+             self.maps[self.position_row-1][self.position_col]=5
+             self.maps[self.position_row+1][self.position_col]=4
+             self.tem_row=self.position_row
+             self.tem_col=self.position_col
+
+
+
+
+#CONTADOR DE CAJAS
+    
 
     def jugar(self):
         self.crear()
